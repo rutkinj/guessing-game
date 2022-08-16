256188365840cc372c5c45930195f39768736a1e
Joe Rutkin <rutkinj@gmail.com>
2022-08-16 14:23:19 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+'use strict'; //require strict syntax
+
+// let whoIs = prompt('Who is you?');
+// console.log(whoIs);
+let a = 10;
+let b = 'dog';
+let c = true;
+
+// evaluate this
+console.log((a + c) + b);
